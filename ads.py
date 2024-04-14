b33b03a89c6d0763d9b24bf6765139510b2e6792
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-04-14 12:58:41 +0000

    streamlit

--- ads.py
+++ ads.py
@@ -7,6 +7,6 @@ css_string = """
             .side-block a.button {margin-bottom:30px; background: #006CFF; color: #fff;font-weight: 500;font-size: 16px;line-height: 20px;padding: 15px;display: inline-block;max-width: 300px;border-radius: 5px; text-decoration:none;   }
             .side-block a.button:hover {background:#000}
 
-            </style>
+            </style
 
             """
